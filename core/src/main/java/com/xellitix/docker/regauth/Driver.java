6d14b2504ed8a6a87e3a02086ec5525388d49dc9
Grayson Kuhns <grayson.kuhns@xellitix.com>
2017-05-12 10:09:37 -0500

    Create core entry point

---
+++ core/src/main/java/com/xellitix/docker/regauth/Driver.java
@@ -0,0 +1,18 @@
+package com.xellitix.docker.regauth;
+
+/**
+ * Application entry point.
+ *
+ * @author Grayson Kuhns
+ */
+public class Driver {
+
+    /**
+     * Application entry point.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
